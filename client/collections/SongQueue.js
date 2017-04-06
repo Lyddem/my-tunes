eab83c0a381f0fb9f799a36ace52a8f3c9f37d5c
Lyddem <lydiademi@gmail.com>
2017-04-05 20:46:01 -0700

    Pass SonqQueue tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,27 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+
+  	this.on('add',function(){
+	  	if(this.length === 1){
+		this.playFirst();
+	};
+  });
+
+  	this.on('ended', function(){
+      this.shift();
+      if(this.length > 0){
+        this.playFirst();
+      }
+  	});
+
+    this.on('dequeue', function(){
+      this.remove(this.at(this.length-1));
+    });
+  },
+
+  playFirst: function(){
+  	this.at(0).play();
+  },
 
 });
